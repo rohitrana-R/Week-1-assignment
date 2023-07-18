1759c1b1a2a5c843ffdd1190c3aefd43e08a4997
ROHIT RANA <137610387+rohitrana-R@users.noreply.github.com>
2023-07-18 20:49:27 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -14,7 +14,14 @@ function isAnagram(str1, str2) {
   {
     console.log("INVALID INPUT");
   }
-   let 
+   let st1 = str1.split('').sort().join('');
+   let st2 = str2.split('').sort().join('');
+  if(st1===st2
+    console.log("True");
+  }
+else{
+  console.log("false");
+}
 }
 
 module.exports = isAnagram;
